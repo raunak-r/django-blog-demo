8c6f3ab239b5d1f87819de5d35b125682f562964
raunak-r <raunakritesh.india@gmail.com>
2019-07-23 11:57:39 +0530

    Create, Update Features Added

--- posts/urls.py
+++ posts/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import url, include
 
 from .views import (create,
+					update,
 					detail,
 					listall)
 
 urlpatterns = [
-	url(r'^create/', create),
+	url(r'^create/$', create),
+	url(r'^(?P<pid>\d+)/update/$', update, name = "update"),
 
 	url(r'^(?P<pid>\d+)/$', detail, name = "detail"),
 	# http://localhost:8000/posts/detail/1/
 	
-	url(r'^listall', listall),
+	url(r'^$', listall),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
 def create(request):
-	return render(request, "index.html", {})
+	form = PostForm(request.POST or None)
+	# if request.method == 'POST':
+	# 	print(request.POST)
+	# 	print(form.cleaned_data.get('title'))
+	
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		# return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		"form" : form,
+	}
+	return render(request, "post_form.html", context)
+
+def update(request, pid=None):
+	instance = get_object_or_404(Post, id = pid)
+	form = PostForm(request.POST or None, instance = instance)
+	
+	if form.is_valid():
+		instance = form.save(commit = False)
+		instance.save()
+		# return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		'title' : instance.title,
+		'content' : instance.content,
+		'form' : form,
+	}
+
+	return render(request, "post_form.html", context)
 
 def detail(request, pid):
 	# This returns an error.
